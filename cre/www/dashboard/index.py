56066efd442af1679d82fd1eaaae058956ddb82a
Ubuntu <ubuntu@ip-172-31-25-109.us-west-2.compute.internal>
2022-03-29 18:13:00 +0000

    add menu styling

--- cre/www/dashboard/index.py
+++ cre/www/dashboard/index.py
@@ -8,6 +8,10 @@ no_cache = True
 def get_context(context):
 	if frappe.session.user=='Guest':
 		frappe.throw(_("You need to be logged in to access this page"), frappe.PermissionError)
-	context.show_sidebar = True
-	context.dashboards = frappe.get_all('Dashboard', fields=['*'])
-	return context
+	
+	context.dashboards = frappe.db.get_all('Dashboard', fields=['*'])
+	context.projects = frappe.db.get_all('Project', fields=['*'])
+	context.workspaces = frappe.db.get_all('Workspace', fields=['*'])
+
+
+		
